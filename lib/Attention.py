4efccddc0780780f7d52f5379a30195008cc022d
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-07-11 13:16:07 -0500

    Update Attention.py

--- lib/Attention.py
+++ lib/Attention.py
@@ -1,1 +1,22 @@
+import numpy as np
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class Attention(nn.Module):
+
+    def __init__(self, Temp, atten_dropout=0.1):
+        self.Temp = Temp
+        self.atten_dropout = atten_dropout
+        self.dropout = nn.Dropout(atten_dropout)
+
+    def fowardpass(self, query, key, value, Mask=None):
+        scores = np.matmul(query / self.Temp, key.T) / math.sqrt(key.size(-1))
+
+        if Mask is not None:
+            scores += -1e9 * Mask
+
+        weights = self.dropout(F.softmax(scores, dim=-1))
+
+        return np.matmul(weights, value), weights
