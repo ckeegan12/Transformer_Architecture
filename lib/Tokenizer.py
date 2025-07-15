1f45ce501c04c6029a99729e7c57d87983fd67f2
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-07-15 10:13:52 -0500

    Update Tokenizer.py

--- lib/Tokenizer.py
+++ lib/Tokenizer.py
@@ -1,1 +1,15 @@
+import nltk
+from nltk.tokenize import RegexpTokenizer
 
+class Word_Tokenizer:
+    def __init__(self, text):
+        self.text = text
+        self.tokenizer = RegexpTokenizer(r'\w+')
+
+    def Tokenize(self):
+        tokens = tokenizer.tokenize(self.text)
+        # Normalize tokens
+        normalized_tokens = [token.lower() for token in tokens]
+        # Add start and stop tokens
+        processed_tokens = ['<START>'] + normalized_tokens + ['<STOP>']
+        return(processed_tokens)
