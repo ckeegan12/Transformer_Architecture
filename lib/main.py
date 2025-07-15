790c88fad4a71d5028a220206e94b9fd8fd5503b
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-07-15 14:40:58 -0500

    Update main.py

--- lib/main.py
+++ lib/main.py
@@ -1,1 +1,18 @@
 
+
+
+# Embedding Layer
+embedding_model = Word2VecEmbedding(tokens=tokenized_input, -----)
+vocab_size = embedding_model.get_vocab_size()
+word_vectors = embedding_model.get_word_vectors()
+
+# Keras embedding layer initialized with the Word2Vec embeddings
+embedding_layer = tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=50,  # Match the vector_size in Word2VecEmbedding
+        weights=[word_vectors],  # Initialize with Word2Vec weights
+        trainable=True
+    )
+    
+# Get the embeddings from the Keras layer
+embeddings = embedding_layer(------)
